f5423f4febd6f18273ee88e29a398ac2396e8168
Jarda Jonas <jarda000@seznam.cz>
2023-03-20 09:52:37 +0100

    first commit

---
+++ src/test/java/com/jarda/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarda.springboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
